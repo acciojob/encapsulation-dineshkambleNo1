40afbf3151ef9730b0bc84225ac10446c6071e90
dinesh <dineshrock153@gmail.com>
2022-12-21 10:36:56 +0530

    msg1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        //crete an obj of class RWOnly in this file
+
+        RWOnly rw = new RWOnly();
+
+//        rw.name = "dinesh";
+        // error : java: name has private access in com.driver.RWOnly;
+
+        rw.setName("dinesh");
+
+        //try, setting a value to name, by directly accessing it using obj and also try printing it
+
+
+    }
 }
